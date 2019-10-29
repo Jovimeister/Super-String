7f1c799b32a4a1d310ba4691424e91c0340cd81b
Jovimeister <55252666+Jovimeister@users.noreply.github.com>
2019-10-29 13:50:16 -0400

    Update superString1.java

--- src/superstring/superString1.java
+++ src/superstring/superString1.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Jovan Gradojevic
+ * 29/10/2019
+ * A program that can does various things to an entered string.
  */
 
 package superstring;
